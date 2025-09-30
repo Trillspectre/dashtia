95c7ae489cd38c12f4a615b1c82be2ecaa0648df
Trillspectre <andrewbullimore@gmail.com>
2025-09-30 15:49:17 +0100

    changed debug setting to False

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com',
                  '127.0.0.1',
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'stats',
-    'home',  # Added after creating the app
+    'home',  
     'channels',
 ]
 
